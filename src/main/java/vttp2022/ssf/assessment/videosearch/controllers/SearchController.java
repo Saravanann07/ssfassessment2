fae141df4e24310a43f7e54490a7bcc2c9147180
Saravanan Munusamy <saravananmunusamy@Saravanans-MacBook-Pro.local>
2022-04-01 14:24:21 +0800

    submission

--- src/main/java/vttp2022/ssf/assessment/videosearch/controllers/SearchController.java
+++ src/main/java/vttp2022/ssf/assessment/videosearch/controllers/SearchController.java
@@ -1,4 +1,33 @@
 package vttp2022.ssf.assessment.videosearch.controllers;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vttp2022.ssf.assessment.videosearch.models.Game;
+import vttp2022.ssf.assessment.videosearch.service.SearchService;
+
+@RestController
+@RequestMapping(path="")
 public class SearchController {
+
+    @Autowired
+    private SearchService searchSvc;
+
+    @GetMapping(path="/games")
+    public String getGame(@RequestParam(required = true) String name, 
+    @RequestParam(name = "count", required = false, defaultValue = "10") Integer count, Model model) {
+        List<Game> gameList = searchSvc.search(name, count);
+
+        if (gameList.isEmpty())
+        return "not_found";
+
+        model.addAttribute("game", gameList);
+        return "search_result";
+    }
+    
 }
--- src/main/java/vttp2022/ssf/assessment/videosearch/service/SearchService.java
+++ src/main/java/vttp2022/ssf/assessment/videosearch/service/SearchService.java
@@ -1,5 +1,86 @@
 package vttp2022.ssf.assessment.videosearch.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import vttp2022.ssf.assessment.videosearch.models.Game;
+
+@Service
 public class SearchService {
+
+    private static final String URL = "https://api.rawg.io/api/games";
+
+    // export RAWG_API_KEY=12c0f38ee89a4e18b5fffae90f05c7b9
+    @Value("${rawg.api.key}")
+    private String apiKey;
+
+    private boolean hasKey;
+
+
+
+    @PostConstruct
+    private void init() {
+        hasKey = null != apiKey;
+        System.out.println(">>>> API key set: " + hasKey);
+    }
+
+    RequestEntity<Void> req = RequestEntity
+            .get(URL)
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+
+    public List<Game> search (String searchString, Integer count){
+        String gameSearch = UriComponentsBuilder.fromUriString(URL)
+            .queryParam("search", searchString)
+            .queryParam("page_size", count)
+            .queryParam("key", apiKey)
+            .toUriString();
+
+        RequestEntity req = RequestEntity
+                .get(gameSearch)
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+
+        RestTemplate template = new RestTemplate();
+
+        ResponseEntity<String> resp = null;
+
+        List<Game> gameList = new ArrayList<>();
+        
+        try {
+        InputStream is = new ByteArrayInputStream(resp.getBody().getBytes());
+        JsonReader reader = Json.createReader(is);
+        JsonObject obj = reader.readObject();
+
+        Game game = new Game();
+        game.setName(obj.getString("name"));
+        //game.setRating(obj.getString("rating"));
+        game.setBackgroundImage(obj.getString("background_image"));
+
+        gameList.add(game);
+        } catch (Exception ex) {
+            System.err.printf("Exception:", ex.getMessage());
+            //return gameList.emmpty();
+
+        }
+        
+        return gameList;
+        
+    }
 
 }
